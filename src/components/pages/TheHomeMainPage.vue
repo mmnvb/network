213b16b758f1fd234353cd3f11b3208e91f4f8b9
muminovbob <bobomurodm2004@gmail.com>
2024-06-28 18:10:49 +0500

    feat: graph text animation

--- src/components/pages/TheHomeMainPage.vue
+++ src/components/pages/TheHomeMainPage.vue
@@ -11,7 +11,7 @@ import TheMiniGraph from "@/components/TheMiniGraph.vue"
     <div class="flex mt-16 w-3/4">
       <div class="w-1/2 flex flex-col justify-evenly">
         <h1 class="text-7xl font-bold text-left leading-snug">
-          Network graph of my university's students
+          Network <span class="boujee-text">graph</span> of my university's students
         </h1>
         <div class="flex flex-col gap-y-16">
           <aside class="text-slate-500">
@@ -32,3 +32,37 @@ import TheMiniGraph from "@/components/TheMiniGraph.vue"
     </div>
   </div>
 </template>
+
+<style scoped>
+.boujee-text {
+  --bg-size: 400%;
+  --color-one: hsl(221, 83%, 53%);
+  --color-two: hsl(261, 83%, 53%);
+  background: linear-gradient(
+                90deg,
+                var(--color-one),
+                var(--color-two),
+                var(--color-one)
+              ) 0 0 / var(--bg-size) 100%;
+  color: transparent;
+  -webkit-background-clip: text;
+  background-clip: text;
+}
+
+@keyframes move-bg {
+  to {
+    background-position: var(--bg-size) 0;
+  }
+}
+
+@media (prefers-reduced-motion: no-preference) {
+  .boujee-text {
+    animation: move-bg 8s linear infinite;
+  }
+  @keyframes move-bg {
+    to {
+      background-position: var(--bg-size) 0;
+    }
+  }
+}
+</style>
